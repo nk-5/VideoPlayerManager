093257c185cb82487e19574739104b016d62069a
nkws <keigo_nakagawa@voyagegroup.com>
2018-07-30 22:20:26 +0900

    create new file, vpm package root

---
+++ video-player-manager/src/main/java/com/example/video_player_manager/Config.java
@@ -0,0 +1,5 @@
+package com.example.video_player_manager;
+
+public class Config {
+    public static final boolean SHOW_LOGS = true;
+}
---
+++ video-player-manager/src/main/java/com/example/video_player_manager/PlayerQueueLock.java
@@ -0,0 +1,4 @@
+package com.example.video_player_manager;
+
+public class PlayerQueueLock {
+}
